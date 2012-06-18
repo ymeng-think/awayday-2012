760f0e44638701dd1b3912d69e426845bbb43304
Meng Yu <ymeng@thoughtworks.com>
2012-06-18 18:45:58 +0800

    [ymeng] create project

---
+++ AwayDay2012/AwayDay2012/MEAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MEAppDelegate.h
+//  AwayDay2012
+//
+//  Created by Meng Yu on 6/18/12.
+//  Copyright (c) 2012 ThoughtWorks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MEAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AwayDay2012/AwayDay2012/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AwayDay2012
+//
+//  Created by Meng Yu on 6/18/12.
+//  Copyright (c) 2012 ThoughtWorks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MEAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MEAppDelegate class]));
+    }
+}
